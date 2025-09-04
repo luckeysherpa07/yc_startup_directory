9b44d6c6634bb444ab9be1fd2aa2eabf0ebd9118
Luckey <luckeysherpa07@gmail.com>
2025-09-04 10:20:28 +0200

    fixed hero section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../../components/Navbar"
 
 export default function Layout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <main className="font-work-sans">
-      <Navbar />
-      {children}
-    </main>
-  )
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+            {children}
+        </main>
+    )
 }
