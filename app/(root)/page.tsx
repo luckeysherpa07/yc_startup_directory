53239b55ef60f5c6c19f3f22ae552e0c39f9aece
Luckey <luckeysherpa07@gmail.com>
2025-09-04 11:27:16 +0200

    added startup card and design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import SearchForm from "../../components/SearchForm"
+import StartupCard from "@/components/StartupCard"
 
 export default async function Home({
     searchParams,
@@ -7,6 +8,22 @@ export default async function Home({
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query
+
+    const posts = [
+        {
+            _createdAt: new Date(),
+            views: 55,
+            author: {
+                _id: 1,
+                name: "Luckey",
+            },
+            _id: 1,
+            description: "This is a description",
+            image: "https://static.vecteezy.com/system/resources/previews/009/171/100/non_2x/demo-symbol-concept-words-demo-on-wooden-blocks-photo.jpg",
+            category: "Robots",
+            title: "We Robots",
+        },
+    ]
     return (
         <>
             <section className="pink_container">
@@ -18,6 +35,20 @@ export default async function Home({
                 </p>
 
                 <SearchForm query={query} />
+            </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType, index: number) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No Startups found</p>
+                    )}
+                </ul>
             </section>
         </>
     )
